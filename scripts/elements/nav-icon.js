b1dd701c6748e0e37f02932f4848f1d5c7164764
Vamba45 <vambafool@bk.ru>
2023-12-01 21:02:02 +0300

    clear filth for burger menu

--- scripts/elements/nav-icon.js
+++ scripts/elements/nav-icon.js
@@ -1,22 +1,5 @@
-const icon = document.getElementById('icon')
-const nav = document.getElementById('nav')
+const navIcon = document.getElementById('icon')
 
-icon.addEventListener('click', function(e) {
+navIcon.addEventListener('click', function(e) {
     this.classList.toggle('active')
-
-    if(nav.classList.contains('active')) {
-        nav.style.transform = "translateY(-800px)"
-        nav.style.transition = "transform 0.2s ease"
-        this.style.pointerEvents = "none"
-
-        setTimeout(() => {
-            this.style.pointerEvents = "auto"
-            nav.classList.toggle('active')
-        }, 200);
-    } else {
-        nav.style.transform = "translateY(0)"
-        nav.style.transition = "transform 0.2s ease"
-                
-        nav.classList.toggle('active')   
-    }
 })
