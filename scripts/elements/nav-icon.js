a9d5887a11af131564e96c460e65e0ec7035e921
Vamba45 <vambafool@bk.ru>
2023-12-02 12:20:47 +0300

    finally finish making burger menu

--- scripts/elements/nav-icon.js
+++ scripts/elements/nav-icon.js
@@ -1,5 +1,8 @@
 const navIcon = document.getElementById('icon')
+const nav = document.getElementById('nav')
 
 navIcon.addEventListener('click', function(e) {
     this.classList.toggle('active')
+    nav.classList.toggle('active')
+    document.body.classList.toggle('active')
 })
