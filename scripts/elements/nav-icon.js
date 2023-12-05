905918813eb9d9e8c7ec2cd59098b5e7fc993411
Vamba45 <vambafool@bk.ru>
2023-12-05 17:59:11 +0300

    finish adaptive aparmnts

--- scripts/elements/nav-icon.js
+++ scripts/elements/nav-icon.js
@@ -2,7 +2,7 @@ const NAV_ICON = document.getElementById('icon')
 const NAV = document.getElementById('nav')
 
 function toggleClass(elements, className) {
-elements.forEach(el => el.classList.toggle(className))
+    elements.forEach(el => el.classList.toggle(className))
 };
 
 NAV_ICON.addEventListener('click', function(e) {
